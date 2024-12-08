48e1e62d21e99cb67f55e4b694f758a7201beace
elvin-mark <munoz.vega.em@gmail.com>
2024-12-08 23:40:08 +0900

    adding resume and functionality to donwload cv button and contact me button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  downloadResume() {
+    const link = document.createElement('a');
+    link.href = 'assets/MyCVEnglish.pdf';
+    link.download = 'Elvin_Resume.pdf';
+    link.click();
+  }
+
+  contactMe() {
+    const link = document.createElement('a');
+    link.href = 'mailto:munoz.vega.em@gmail.com';
+    link.click();
+  }
+}
